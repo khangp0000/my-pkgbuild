aa34d39a243c9c8316dd404024268bd81ed1c215
Khang Phan <khangp0000@gmail.com>
2024-07-07 02:36:24 -0700

    Add script to update firewalld source for cloudflare and lan

--- cloudflare-refresh/cloudflare-refresh.py
+++ cloudflare-refresh/cloudflare-refresh.py
@@ -6,6 +6,7 @@ import tempfile
 import shutil
 import subprocess
 import os
+import re
 
 def move_with_perm(src, dst):
     stat = os.stat(dst)
@@ -17,27 +18,45 @@ def move_with_perm(src, dst):
 print("Downloading cloudflare ips...")
 ipsv4 = requests.get("https://www.cloudflare.com/ips-v4/").text.splitlines()
 ipsv6 = requests.get("https://www.cloudflare.com/ips-v6/").text.splitlines()
-
-ips = map(lambda s: {'@address': s}, ipsv4 + ipsv6)
+ipset = set(ipsv4 + ipsv6)
 
 with open('/etc/firewalld/zones/cloudflare.xml', "r") as fd:
     firewalld_cf = xmltodict.parse(fd.read())
-    
-firewalld_cf['zone']['source'] = ips
 
-print("Updating firewalld config...")
-with tempfile.NamedTemporaryFile(mode='w', prefix="cloudflare-refresh", delete=False) as temp_firewalld:
-    temp_firewalld.write(xmltodict.unparse(firewalld_cf,short_empty_elements=True))
-move_with_perm(temp_firewalld.name, "/etc/firewalld/zones/cloudflare.xml")
+if isinstance(firewalld_cf['zone']['source'], dict):
+    currentAddrs = set(firewalld_cf['zone']['source']['@address'])
+elif isinstance(firewalld_cf['zone']['source'], list):
+    currentAddrs = set(k['@address'] for k in firewalld_cf['zone']['source'])
 
-print("Updating nginx config...")
-with tempfile.NamedTemporaryFile(mode='w', prefix="cloudflare-refresh", delete=False) as temp_nginx:
-    temp_nginx.writelines(map(lambda s: f"set_real_ip_from {s};\n", ipsv4 + ipsv6))
-move_with_perm(temp_nginx.name, "/etc/nginx/http/locations/cloudflare_ips.conf")
+if (currentAddrs != ipset):
+    firewalld_cf['zone']['source'] = map(lambda s: {'@address': s}, ipsv4 + ipsv6)
 
-print("Reload firewalld...")
-subprocess.run(['/usr/bin/firewall-cmd', '--reload'], check=True)
-print("Reload fail2ban...")
-subprocess.run(['systemctl', 'restart', 'fail2ban.service'], check=True)
-print("Reload nginx...")
-subprocess.run(['nginx', '-s', 'reload'], check=True)
+    print("Updating firewalld config...")
+    with tempfile.NamedTemporaryFile(mode='w', prefix="cloudflare-refresh", delete=False) as temp_firewalld:
+        temp_firewalld.write(xmltodict.unparse(firewalld_cf,short_empty_elements=True))
+    move_with_perm(temp_firewalld.name, "/etc/firewalld/zones/cloudflare.xml")
+    print("Reload firewalld...")
+    subprocess.run(['/usr/bin/firewall-cmd', '--reload'], check=True)
+    print("Reload fail2ban...")
+    subprocess.run(['systemctl', 'restart', 'fail2ban.service'], check=True)
+else:
+    print("Firewalld does not need update...")
+
+current = set()
+pattern = re.compile("set_real_ip_from (?P<cidr>.+);")
+with open('/etc/nginx/http/locations/cloudflare_ips.conf', "r") as fd:
+    for line in fd.readlines():
+        match = pattern.fullmatch(line.strip())
+        if match is None:
+            raise Exception("cloudflare_ips.conf invalid")
+        current.add(match.group('cidr'))
+        
+if current != ipset:
+    print("Updating nginx config...")
+    with tempfile.NamedTemporaryFile(mode='w', prefix="cloudflare-refresh", delete=False) as temp_nginx:
+        temp_nginx.writelines(map(lambda s: f"set_real_ip_from {s};\n", ipsv4 + ipsv6))
+    move_with_perm(temp_nginx.name, "/etc/nginx/http/locations/cloudflare_ips.conf")
+    print("Reload nginx...")
+    subprocess.run(['nginx', '-s', 'reload'], check=True)
+else:
+    print("nginx does not need update...")
---
+++ lan-firewall/lan-firewall.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import ipaddress
+import socket
+import psutil
+import math
+import xmltodict
+import shutil
+import subprocess
+import tempfile
+import os
+
+def move_with_perm(src, dst):
+    stat = os.stat(dst)
+    shutil.copymode(dst, src)
+    shutil.move(src, dst)
+    shutil.chown(dst, stat.st_uid, stat.st_gid)
+
+addrs = psutil.net_if_addrs()['enp4s0']
+sanitized_addrs = []
+for addr in addrs:
+    if addr.family == socket.AF_INET or addr.family == socket.AF_INET6:
+        val = int.from_bytes(ipaddress.ip_address(addr.netmask).packed, byteorder='little')
+        prefix_len = int(math.log2(val + 1))
+        cidr = ipaddress.ip_network(f"{addr.address}/{prefix_len}", strict=False)
+        if cidr.version == 4 or cidr.is_global:
+            sanitized_addrs.append(str(cidr))
+            
+ipset = set(sanitized_addrs)
+
+with open('/etc/firewalld/zones/home.xml', "r") as fd:
+    firewalld_cf = xmltodict.parse(fd.read())
+print(firewalld_cf['zone']['source'])
+
+if isinstance(firewalld_cf['zone']['source'], dict):
+    currentAddrs = set(firewalld_cf['zone']['source']['@address'])
+elif isinstance(firewalld_cf['zone']['source'], list):
+    currentAddrs = set(k['@address'] for k in firewalld_cf['zone']['source'])
+
+if (currentAddrs != ipset):
+    firewalld_cf['zone']['source'] = map(lambda s: {'@address': s}, sanitized_addrs)
+
+    print("Updating firewalld config...")
+    with tempfile.NamedTemporaryFile(mode='w', prefix="lan-firewall", delete=False) as temp_firewalld:
+        temp_firewalld.write(xmltodict.unparse(firewalld_cf,short_empty_elements=True))
+    move_with_perm(temp_firewalld.name, "/etc/firewalld/zones/home.xml")
+    print("Reload firewalld...")
+    subprocess.run(['/usr/bin/firewall-cmd', '--reload'], check=True)
+    print("Reload fail2ban...")
+    subprocess.run(['systemctl', 'restart', 'fail2ban.service'], check=True)
+else:
+    print("Firewalld does not need update...")
